a455f04f30fda90a765b12c99eac4603fee2d8af
Luca Luraschi <luca.luraschi@dellorto.it>
2019-05-09 22:08:15 +0200

    Add project files.

---
+++ WPF_MVVM_FCAMUSO/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_MVVM_FCAMUSO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+        private Models.IPersoneService  personeService = null;
+
+        public MainWindow(Models.IPersoneService personeService)
+        {
+            InitializeComponent();
+            this.personeService = personeService;
+            cmbPersone.ItemsSource = personeService.Persone;
+            cmbPersone.DisplayMemberPath = "Cognome";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            salutaMi();
+        }
+
+        private void salutaMi()
+        {
+            if (cmbPersone.SelectedItem!=null)
+            {
+               var persona = (Models.Persona)cmbPersone.SelectedItem;
+
+                txtSaluto.Text = $"Ciao {persona.Nome} {persona.Cognome}";
+
+            }
+        }
+    }
+}
---
+++ WPF_MVVM_FCAMUSO/Models/PersoneService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace WPF_MVVM_FCAMUSO.Models
+{
+    public interface IPersoneService
+    {
+        IList<Persona> Persone { get; }
+    }
+
+    internal class PersoneService
+    {
+        private readonly List<Persona> _persone = null;
+        public PersoneService()
+        {
+            _persone = new List<Persona>
+            {
+                new Persona() { Nome = "Luca", Cognome = "Luraschi" },
+                new Persona() { Nome = "Mario", Cognome = "Marchetti" },
+                new Persona() { Nome = "Barbara", Cognome = "Cavalli" },
+                new Persona() { Nome = "Beatrice", Cognome = "Bianchi" },
+                new Persona() { Nome = "Roberto", Cognome = "Valtorta" },
+                new Persona() { Nome = "Adriano", Cognome = "Pappalardo" },
+                new Persona() { Nome = "Marco", Cognome = "Muliere" },
+                new Persona() { Nome = "Daniele", Cognome = "Albertini" }
+            };
+
+        }
+
+
+
+        public IList<Persona> Persone => _persone;
+    }
+
+}
+
